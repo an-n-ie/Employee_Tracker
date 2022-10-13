7fc5a8ae2aa8c4b2fc32d488e0675270ec142d84
Annie Hoang <anniehoang.anh@gmail.com>
2022-10-13 19:16:16 -0400

    seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+USE employee_db;
+
+INSERT INTO department(name)
+VALUES ("Sales"),
+("Engineering"),
+("Finance"),
+("Legal");
+
+INSERT INTO role(title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1),
+("Salesperson", 80000, 1),
+("Lead Engineer", 150000, 2);
+
+INSERT INTO employee(first_name, last_name, role_id)
+VALUES ("John", "Doe", 1),
+("Mike","Chan", 2);
+
+UPDATE employee SET manager_id=1 WHERE id=2;
