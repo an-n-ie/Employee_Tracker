35ef38eee06c38b448e0b095fa670d6b938affb6
Annie Hoang <anniehoang.anh@gmail.com>
2022-10-16 11:48:22 -0400

    view roles

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const menuQuestion=[
     {
         type:"list",
         name:"menu",
-        message:"choose the following option:",
-        choices:["view all departments","view all roles","view all employees","add a department","add a role","add an employee","update an employee role"]
+        message:"What would you like to do?",
+        choices:["View all departments","View all roles","View all employees","Add a department","Add a role","Add an employee","Update an employee role"]
     }
 ]
 
@@ -23,52 +23,65 @@ const menuQuestion=[
 function menu(){
   inquirer.prompt(menuQuestion)
   .then(response=>{
-    if(response.menu==="view all employees"){
-        viewEmployees()
+    if(response.menu==="View all employees"){
+        viewEmployee()
     }
-    else if(response.menu==="view all departments"){
-        viewDepartments()
+    else if(response.menu==="View all departments"){
+        viewDepartment()
     }
-    else if(response.menu==="add an employee"){
-        addEmployees()
+    else if(response.menu==="View all roles"){
+        viewRole()
+    }
+    else if(response.menu==="Add a role"){
+        addRole()
+    }
+    else if(response.menu==="Add an employee"){
+        addEmployee()
     }
 
   })
     
 }
-function viewDepartments(){
+function viewDepartment(){
     db.query("select* from department", (err, data)=>{
         console.table(data)
         menu()
     })
 }
 
-function addEmployees(){
+function viewRole(){
+    db.query("select* from role", (err, data)=>{
+        console.table(data)
+        menu()
+    })
+}
+
+function addEmployee(){
     db.query("select title as name, id as value from role", (er, roleData)=>{
 
-           db.query(`select CONCAT(first_name, " " , last_name) as name,  id as value from employee where  manager_id is null `, (err, managerData)=>{
+           db.query(`select CONCAT(first_name, " " , last_name) as name, id as value from employee where manager_id is null `, (err, managerData)=>{
             const employeeAddQuestions=[
                 {
                     type:"input",
                     name:"first_name",
-                    message:"What is your first name?",
+                    message:"What is the employee's first name?",
             
                 },
                 {
                     type:"input",
                     name:"last_name",
-                    message:"What is your first name?",
+                    message:"What is the employee's last name?",
             
                 },
                 {
                     type:"list",
                     name:"role_id",
-                    message:"Choose the following role title",
+                    message:"Choose the role:",
                     choices:roleData
                 },{
                     type:"list",
                     name:"manager_id",
-                    message:"Choose the following manager",
+                    message:"Choose the manager:",
                     choices:managerData
                 }
             
@@ -77,13 +90,13 @@ function addEmployees(){
                 const parameters=[response.first_name,response.last_name,response.role_id, response.manager_id]
                 db.query("INSERT INTO employee (first_name,last_name,role_id,manager_id)VALUES(?,?,?,?)",parameters,(err, data)=>{
 
-                    viewEmployees()
+                    viewEmployee()
                 })
             })
            })
     })
 }
-function viewEmployees(){
+function viewEmployee(){
 db.query(`
 SELECT 
 employee.id,
@@ -96,10 +109,9 @@ CONCAT(mgr.first_name, " " , mgr.last_name) as manager
 FROM employee
 LEFT JOIN role ON role.id= employee.role_id
 LEFT JOIN department ON role.department_id=department.id
-LEFT JOIN employee as mgr ON employee.manager_id =  mgr.id
+LEFT JOIN employee as mgr ON employee.manager_id = mgr.id
 
 `,  (err,data)=>{
-    //   console.log(data)
     console.table(data)
 
     menu()
